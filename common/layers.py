de8959b060df71c15a16d7e1cdf714883ebdc529
bluepark-sk <blue4park@gmail.com>
2022-02-16 23:35:02 +0900

    CH5. 순환 신경망(RNN)

--- common/layers.py
+++ common/layers.py
@@ -90,3 +90,48 @@ class SigmoidWithLoss:
 
         dx = (self.y - self.t) * dout / batch_size
         return dx
+
+class Embedding:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+    
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+    
+    def backward(self, dout):
+        dW, = self.grads
+        dW[...] = 0
+
+        # dW[self.idx] = dout
+        for i, word_id in enumerate(self.idx):
+            dW[word_id] += dout[i]
+        # np.add.at(dW, self.idx, dout)
+        return None
+
+class Affine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+    
+    def forward(self, x):
+        W, b = self.params
+        out = np.matmul(x, W) + b
+        self.x = x
+        return out
+    
+    def backward(self, dout):
+        W, b = self.params
+        dx = np.matmul(dout, W.T)
+        dW = np.matmul(self.x.T, dout)
+        db = np.sum(dout, axis=0)
+
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+
+        return dx
